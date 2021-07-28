54e4cbfd07a7dcf6b02752dfcca514cace7d5032
Dianne <mdianne.gabriel@gmail.com>
2021-07-28 13:17:13 -0400

    Updated play and client

--- play.js
+++ play.js
@@ -1,7 +1,8 @@
 const net = require("net");
+const connect = require('./client');
 
 // establishes a connection with the game server
-const connect = function () {
+// const connect = function () {
 
   const conn = net.createConnection({
     host: '135.23.223.133',
@@ -9,13 +10,13 @@ const connect = function () {
   });
 
   // interpret incoming data as text
-  conn.setEncoding("utf8");
+  // conn.setEncoding("utf8");
 
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-  return conn;
-};
+  // conn.on("data", (data) => {
+  //   console.log(data);
+  // });
+  // return conn;
+// };
 
 console.log("Connecting ...");
 connect();
